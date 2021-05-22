7adc06681379e9e5322a2fbbf25519ddbe9e21d5
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-05-22 22:20:24 +0530

    feat: add card component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "tachyons";
+import Card from "./components/Card";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { robots } from "./robots";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Card robot={robots[0]} />
+    <Card robot={robots[1]} />
+    <Card robot={robots[2]} />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
