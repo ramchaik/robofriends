fb9b52571430f7df5b1bef1c6cee523384d4c50e
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-05-23 10:26:47 +0530

    chore: cleanup

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
+import CardList from "../components/CardList";
+import Scroll from "../components/Scroll";
+import SearchBox from "../components/SearchBox";
 import "./App.css";
-import CardList from "./components/CardList";
-import Scroll from "./components/Scroll";
-import SearchBox from "./components/SearchBox";
 
 class App extends Component {
   constructor() {
@@ -29,11 +29,9 @@ class App extends Component {
       robot.name.toLowerCase().includes(searchfield.toLowerCase())
     );
 
-    if (robots.length === 0) {
-      return <h1 className="tc">Loading</h1>;
-    }
-
-    return (
+    return !robots.length ? (
+      <h1 className="tc">Loading</h1>
+    ) : (
       <div className="tc">
         <h1 className="f1">RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "tachyons";
-import App from "./App";
+import App from "./containers/App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
