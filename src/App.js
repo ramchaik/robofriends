e6ce6f3569c6a683b72dc99c298998fe65db0b71
Vaibhav Singh <0vaibhavsingh0@gmail.com>
2021-05-23 10:01:35 +0530

    feat: fetch robots via API

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React, { Component } from "react";
 import "./App.css";
 import CardList from "./components/CardList";
 import SearchBox from "./components/SearchBox";
-import { robots } from "./robots";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots,
+      robots: [],
       searchfield: "",
     };
+  }
+
+  componentWillMount() {
+    fetch(`https://jsonplaceholder.typicode.com/users`)
+      .then((res) => res.json())
+      .then((robots) => this.setState({ robots }));
   }
 
   onSearchChange = (event) => {
@@ -22,6 +27,10 @@ class App extends Component {
     const filteredRobots = robots.filter((robot) =>
       robot.name.toLowerCase().includes(searchfield.toLowerCase())
     );
+
+    if (robots.length === 0) {
+      return <h1 className="tc">Loading</h1>;
+    }
 
     return (
       <div className="tc">
